df469bc071d3f4cc19f0f1d267c56b8f3bdb3e15
AlirezaTimas <126695536+AlirezaTimas@users.noreply.github.com>
2023-04-26 16:02:36 +0430

    small exception handling commited

--- Program.cs
+++ Program.cs
@@ -1,34 +1,45 @@
-﻿namespace Vazne_calculator_app_final
+﻿using System.Runtime.CompilerServices;
+
+namespace Vazne_calculator_app_final
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Meghdar vazne ra vared konid?: ");
-                int userVAZN = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the amount of weight: ");
+            try
+            {
+                int userWeight = int.Parse(Console.ReadLine());
 
-                int vazne20kilooyi = 0;
-                int vazne10kilooyi = 0;
-                int vazne5kilooyi = 0;
-                int vazne2kilooyi = 0;
-                int vazne1kilooyi = 0;
-                userVAZN /= 2;
+                int var20kgweight = 0;
+                int var10kgweight = 0;
+                int var5kgweight = 0;
+                int var2kgweight = 0;
+                int var1kgweight = 0;
+                userWeight /= 2; //Have to use half the weight , because this app is calculating required weight at "Each end of barbell bar"
 
-                vazne20kilooyi = userVAZN / 20;
-                userVAZN %= 20;
-                vazne10kilooyi = userVAZN / 10;
-                userVAZN %= 10;
-                vazne5kilooyi = userVAZN / 5;
-                userVAZN %= 5;
-                vazne2kilooyi = userVAZN / 2;
-                userVAZN %= 2;
-                vazne1kilooyi = userVAZN / 1;
-                userVAZN %= 1;
-                Console.WriteLine("vazne 20 kilooyi: " + vazne20kilooyi + "\n vazne 10 kilooyi : " + vazne10kilooyi + " \n vazne 5 kilooyi: " + vazne5kilooyi +
-                    " \n vazne 2 kilooyi: " + vazne2kilooyi + " \n vazne 1 kilooyi: " + vazne1kilooyi);
+                var20kgweight = userWeight / 20;
+                userWeight %= 20;
+                var10kgweight = userWeight / 10;
+                userWeight %= 10;
+                var5kgweight = userWeight / 5;
+                userWeight %= 5;
+                var2kgweight = userWeight / 2;
+                userWeight %= 2;
+                var1kgweight = userWeight / 1;
+                userWeight %= 1;
+                Console.WriteLine(" 20 kg weight: " + var20kgweight + "\n 10 kg weight: " + var10kgweight + " \n 5 kg weight: " + var5kgweight +
+                    " \n 2 kg weight: " + var2kgweight + " \n 1 kg weight: " + var1kgweight);
                 Console.ReadKey();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("You can only enter numbers not characters, Restart the app and try again !");
+            }
+
         }
     }
+}
 
 
